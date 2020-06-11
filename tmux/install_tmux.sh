9280e8fd5f92440f85d9dbe212b7f468a4411ec4
tony <1241484989@qq.com>
2020-06-11 16:45:00 +0800

    :bug: Fix bug that tmux path error.

--- tmux/install_tmux.sh
+++ tmux/install_tmux.sh
@@ -1,20 +1,23 @@
 #! /bin/bash
 
-TMUX_PATH=$(dirname $0)
+TMUX_PATH=$(pwd)
 
 # libevent
-tar -zxvf $TMUX_PATH/libevent-2.1.8-stable.tar.gz
+tar -zxvf $TMUX_PATH/libevent-2.1.8-stable.tar.gz -C $TMUX_PATH
 cd $TMUX_PATH/libevent-2.1.8-stable
 ./configure && make
 make install
 ln -s /usr/local/lib/libevent-2.1.so.6 /usr/lib64/libevent-2.1.so.6
 
 # tmux
-tar -zxvf $TMUX_PATH/tmux-2.6.tar.gz
+tar -zxvf $TMUX_PATH/tmux-2.6.tar.gz -C $TMUX_PATH
 cd $TMUX_PATH/tmux-2.6
 ./configure 
 make
 make install
+if [ -f /usr/bin/tmux ]; then
+    rm -f /usr/bin/tmux
+fi
 ln -s /usr/local/bin/tmux /usr/bin/tmux
 
 # tmux conf
